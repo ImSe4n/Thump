e7cb15bbc7dbd1370a8afe54a7e72baf153080af
Sean Nie <81334381+ImSe4n@users.noreply.github.com>
2026-06-30 23:27:04 -0400

    adhere to msfs naming policies and add icons

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
-"C:\MSFS_SDK\Tools\bin\fspackagetool.exe" "Thump\Build\thump.xml"
-copy /Y "Thump\Build\Packages\thump.spb" "Thump\InGamePanels"
+"C:\MSFS_SDK\Tools\bin\fspackagetool.exe" "Build\sn-thump.xml" -nopause
+xcopy /E /I /Y "html_ui" "Build\Packages\sn-thump\html_ui"
+copy /Y "icon.png" "Build\Packages\sn-thump\icon.png"
+"C:\MSFS_SDK\Tools\bin\fspackagetool.exe" "Build\sn-thump.xml" -nopause
